f8669aae6f4f773e998adfbc27fd5247736e159f
ilambharathi1 <ilambharathi2k.aadhi@gmail.com>
2024-03-28 01:08:49 +0530

    updated myapps

---
+++ mysite/urls.py
@@ -0,0 +1,36 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.contrib.auth.models import User
+from django_otp.admin import OTPAdminSite
+from django_otp.plugins.otp_totp.models import TOTPDevice
+from django_otp.plugins.otp_totp.admin import TOTPDeviceAdmin
+
+
+
+class OTPAdmin(OTPAdminSite):
+    pass  
+
+admin_site = OTPAdmin(name='OTPAdmin')
+admin_site.register(User)
+admin_site.register(TOTPDevice, TOTPDeviceAdmin)
+
+urlpatterns = [
+     path('admin/', admin_site.urls),
+	# path('admin/', admin.site.urls),     #This the Default admin panel .Use this panel to register the TOTP device
+    path('',include('myapp.urls')),
+]
